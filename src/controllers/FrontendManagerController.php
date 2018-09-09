d525494b84e1bae8b67133e0bf15e00eca912b0f
Andy Adiwidjaja <adiwidjaja@gmail.com>
2018-09-09 23:51:10 +0200

    Initial import of basic classes.

---
+++ src/controllers/FrontendManagerController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ATW\SilverstripeFrontendManager\Controllers;
+
+use SilverStripe\Control\Controller;
+
+class FrontendManagerController extends Controller {
+
+}
---
+++ src/extensions/ManageableObjectExtension.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ATW\SilverstripeFrontendManager\Extensions;
+
+use SilverStripe\ORM\DataExtension;
+
+class ManageableObjectExtension extends DataExtension {
+
+}
